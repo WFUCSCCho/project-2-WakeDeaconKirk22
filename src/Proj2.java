398b3891fcfdc390327ff28b3af542a7e0df55c3
Kennedy <166327402+CodewithJoshandEric@users.noreply.github.com>
2024-11-05 10:58:27 -0500

    added 3 codes from the previous project along with NBA file. Made changes to AVLTree.java and Proj2.java. Added Comments and added the images to README.md and add file descriptions to each file

--- src/Proj2.java
+++ src/Proj2.java
@@ -16,8 +16,8 @@ public class Proj2 {
 
     public static void main(String[] args) throws IOException {
         // Use command line arguments to specify the input file
-        ArrayList<String>NBAPlayer = new ArrayList<>();
-        ArrayList<String>Backup=new ArrayList<>();
+        ArrayList<NBAPlayer> Player = new ArrayList<>();
+        ArrayList<NBAPlayer> Backup=new ArrayList<>();
         BST<NBAPlayer> mybst = new BST<>();
         AvlTree<NBAPlayer> avlTree = new AvlTree<>();
 
@@ -41,32 +41,56 @@ public class Proj2 {
         // ignore first line
         inputFileNameScanner.nextLine();
         for(int i = 0; i<numLines; i++) {
-            NBAPlayer.add(inputFileNameScanner.nextLine());
+            String temp = inputFileNameScanner.nextLine();
+            String[] command = temp.split(",");
+            NBAPlayer player = new NBAPlayer( Integer.parseInt(command[0]), // id
+                    command[1],//name
+                    command[2], // team
+                    Double.parseDouble(command[3]), // age
+                    Double.parseDouble(command[4]), // player height
+                    Double.parseDouble(command[5]), // player weight
+                    command[6], // college name
+                    command[7], // country name
+                    command[8], // draft year
+                    command[9], // draft round
+                    command[10], // draft number
+                    command[11], // games played (gp)
+                    Double.parseDouble(command[12]), // points
+                    Double.parseDouble(command[13]), // rebounds
+                    Double.parseDouble(command[14]), // assists
+                    Double.parseDouble(command[15]), // net rating
+                    Double.parseDouble(command[16]), // offensive rebound percentage
+                    Double.parseDouble(command[17]), // defensive rebound percentage
+                    Double.parseDouble(command[18]), // usage percentage
+                    Double.parseDouble(command[19]), // assist percentage
+                    Double.parseDouble(command[20]),
+                    command[21]);
+            Player.add(player);
         }
         //backup arrayList
-        Backup=NBAPlayer;
+        Backup = Player;
 
 	// FINISH ME
 
 // Takes the intial
 
 
-     Collections.shuffle(NBAPlayer);
+     Collections.shuffle(Player);
 //Gives the rand insertion time
-    long avlRandInsertTime = insertTime(avlTree, NBAPlayer);
-    long bstRandInsertTime = insertTime(mybst, NBAPlayer);
+    long avlRandInsertTime = insertTime(avlTree, Player);
+    long bstRandInsertTime = insertTime(mybst, Player);
 
     long bstSearchTimeRandom = searchTime(mybst,Backup);
     long avlSearchTimeRandom = searchTime(avlTree,Backup);
 
         //Gives the sorted Search and insertion times
-    Collections.sort(NBAPlayer);
-    long  bstSortedSearchTime = searchTime(mybst,NBAPlayer);
-    long avlSortedSearchTime = searchTime(avlTree,NBAPlayer);
+    Collections.sort(Player);
+    long  bstSortedSearchTime = searchTime(mybst,Player);
+    long avlSortedSearchTime = searchTime(avlTree,Player);
 
 
-        long avlSortedInsertTime = insertTime(avlTree,NBAPlayer);;
-        long bstSortedInsertTime = insertTime(mybst,NBAPlayer);
+        long avlSortedInsertTime = insertTime(avlTree,Player);;
+        long bstSortedInsertTime = insertTime(mybst,Player);
 
 
 
@@ -90,134 +114,47 @@ String outFilePath = "./output.txt";
 
 
     }
-    private static long insertTime(BST<NBAPlayer> bst , ArrayList<String>NBAPlayer) {
+    private static long insertTime(BST<NBAPlayer> bst , ArrayList<NBAPlayer>NBAPlayer) {
         // Funnction call to count the insertion time for BST
         long time = System.nanoTime(); //start time
 
             for(int i = 0; i < NBAPlayer.size(); i++) {
-                String[] command = NBAPlayer.get(i).split(",");
-                NBAPlayer player = new NBAPlayer( Integer.parseInt(command[0]), // id
-                        command[1],//name
-                        command[2], // team
-                        Double.parseDouble(command[3]), // age
-                        Double.parseDouble(command[4]), // player height
-                        Double.parseDouble(command[5]), // player weight
-                        command[6], // college name
-                        command[7], // country name
-                        command[8], // draft year
-                        command[9], // draft round
-                        command[10], // draft number
-                        command[11], // games played (gp)
-                        Double.parseDouble(command[12]), // points
-                        Double.parseDouble(command[13]), // rebounds
-                        Double.parseDouble(command[14]), // assists
-                        Double.parseDouble(command[15]), // net rating
-                        Double.parseDouble(command[16]), // offensive rebound percentage
-                        Double.parseDouble(command[17]), // defensive rebound percentage
-                        Double.parseDouble(command[18]), // usage percentage
-                        Double.parseDouble(command[19]), // assist percentage
-                        Double.parseDouble(command[20]),
-                        command[21]);
-                bst.insert(player);// inserts the player object
+
+                bst.insert(NBAPlayer.get(i));// inserts the player object
 
         }
         long end = System.nanoTime();
         return end - time; // total time
     }
 
-    private static long insertTime(AvlTree<NBAPlayer> AvlTree, ArrayList<String>NBAPlayer) {
+    private static long insertTime(AvlTree<NBAPlayer> AvlTree, ArrayList<NBAPlayer>NBAPlayer) {
         long time = System.nanoTime(); //start time
         // Funnction call to count the insertion time for AVL
         for(int i = 0; i < NBAPlayer.size(); i++) {
-            String[] command = NBAPlayer.get(i).split(",");
-            NBAPlayer player = new NBAPlayer( Integer.parseInt(command[0]), // id
-                    command[1],//name
-                    command[2], // team
-                    Double.parseDouble(command[3]), // age
-                    Double.parseDouble(command[4]), // player height
-                    Double.parseDouble(command[5]), // player weight
-                    command[6], // college name
-                    command[7], // country name
-                    command[8], // draft year
-                    command[9], // draft round
-                    command[10], // draft number
-                    command[11], // games played (gp)
-                    Double.parseDouble(command[12]), // points
-                    Double.parseDouble(command[13]), // rebounds
-                    Double.parseDouble(command[14]), // assists
-                    Double.parseDouble(command[15]), // net rating
-                    Double.parseDouble(command[16]), // offensive rebound percentage
-                    Double.parseDouble(command[17]), // defensive rebound percentage
-                    Double.parseDouble(command[18]), // usage percentage
-                    Double.parseDouble(command[19]), // assist percentage
-                    Double.parseDouble(command[20]),
-                    command[21]);
-            AvlTree.insert(player); // inserts the player object
+
+            AvlTree.insert(NBAPlayer.get(i)); // inserts the player object
         }
         long end = System.nanoTime(); // total time
         return end - time;
     }
 
-private static long searchTime(AvlTree<NBAPlayer> AvlTree, ArrayList<String>NBAPlayer) {
+private static long searchTime(AvlTree<NBAPlayer> AvlTree, ArrayList<NBAPlayer>NBAPlayer) {
     // Funnction call to count the search time for AVL
         long time = System.nanoTime(); //start time
 
         for(int i = 0; i < NBAPlayer.size(); i++) {
-            String[] command = NBAPlayer.get(i).split(",");
-            NBAPlayer player = new NBAPlayer( Integer.parseInt(command[0]), // id
-                    command[1],//name
-                    command[2], // team
-                    Double.parseDouble(command[3]), // age
-                    Double.parseDouble(command[4]), // player height
-                    Double.parseDouble(command[5]), // player weight
-                    command[6], // college name
-                    command[7], // country name
-                    command[8], // draft year
-                    command[9], // draft round
-                    command[10], // draft number
-                    command[11], // games played (gp)
-                    Double.parseDouble(command[12]), // points
-                    Double.parseDouble(command[13]), // rebounds
-                    Double.parseDouble(command[14]), // assists
-                    Double.parseDouble(command[15]), // net rating
-                    Double.parseDouble(command[16]), // offensive rebound percentage
-                    Double.parseDouble(command[17]), // defensive rebound percentage
-                    Double.parseDouble(command[18]), // usage percentage
-                    Double.parseDouble(command[19]), // assist percentage
-                    Double.parseDouble(command[20]),
-                    command[21]);
-            AvlTree.contains(player);  // searches the player object
+
+            AvlTree.contains(NBAPlayer.get(i));  // searches the player object
         }
     long end = System.nanoTime(); // total time
     return end - time;
 }
-private static long searchTime(BST<NBAPlayer>bst ,ArrayList<String>NBAPlayer) {
+private static long searchTime(BST<NBAPlayer>bst ,ArrayList<NBAPlayer>NBAPlayer) {
     // Funnction call to count the search time for BST
         long time = System.nanoTime(); //start time
-        for(int i = 0; i < NBAPlayer.size(); i++) { String[] command = NBAPlayer.get(i).split(",");
-            NBAPlayer player = new NBAPlayer( Integer.parseInt(command[0]), // id
-                    command[1],//name
-                    command[2], // team
-                    Double.parseDouble(command[3]), // age
-                    Double.parseDouble(command[4]), // player height
-                    Double.parseDouble(command[5]), // player weight
-                    command[6], // college name
-                    command[7], // country name
-                    command[8], // draft year
-                    command[9], // draft round
-                    command[10], // draft number
-                    command[11], // games played (gp)
-                    Double.parseDouble(command[12]), // points
-                    Double.parseDouble(command[13]), // rebounds
-                    Double.parseDouble(command[14]), // assists
-                    Double.parseDouble(command[15]), // net rating
-                    Double.parseDouble(command[16]), // offensive rebound percentage
-                    Double.parseDouble(command[17]), // defensive rebound percentage
-                    Double.parseDouble(command[18]), // usage percentage
-                    Double.parseDouble(command[19]), // assist percentage
-                    Double.parseDouble(command[20]),
-                    command[21]);
-            bst.search(player);  // searches the player object
+        for(int i = 0; i < NBAPlayer.size(); i++) {
+
+            bst.search(NBAPlayer.get(i));  // searches the player object
         }
     long end = System.nanoTime();
     return end - time; // total time
